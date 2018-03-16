888c417fdc894601938b18a1f4872080ab62636e
Aurora Nou <auroranou@gmail.com>
2018-03-16 19:29:22 -0400

    use block structure

--- 1-1/1-8.scm
+++ 1-1/1-8.scm
@@ -10,23 +10,24 @@
 (define (cube x) (* x x x))
 
 (define (cube-root x)
-  (cube-iter 1.0 x))
 
-(define (cube-iter guess x)
-  (if (good-enough? guess x)
+  (define (cube-iter guess x)
+    (if (good-enough? guess x)
     guess
     (cube-iter 
       (improve guess x) 
-      x)))
+        x)))
 
-(define (good-enough? guess x)
-  (< 
-    (abs (- (cube guess) x))
-    (* guess 0.001)))
+  (define (good-enough? guess x)
+    (< 
+      (abs (- (cube guess) x))
+      (* guess 0.001)))
 
-(define (improve guess x)
-  (/
-    (+
-      (/ x (square guess))
-      (* 2 guess))
-    3))
+  (define (improve guess x)
+    (/
+      (+
+        (/ x (square guess))
+        (* 2 guess))
+        3))
+
+  (cube-iter 1.0 x))
